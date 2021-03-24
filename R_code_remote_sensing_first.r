a31b8b62320b5d7fff288981dad2762406335d8c
TLBacchilega <80838223+TLBacchilega@users.noreply.github.com>
2021-03-24 17:36:43 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -3,6 +3,6 @@
 install.packages("raster")#(argomento)
 library(raster)
 setwd("C:/lab/")# sistema operativo windows 
-p224r63_2011 <- brick("p224r63_2011_masked.grd")#funzione brick che permette di importare immagini satellitari
+p224r63_2011 <- brick("p224r63_2011_masked.grd")#funzione brick permette di importare immagini satellitari
 p224r63_2011#info sul file
 plot(p224r63_2011)#funzione che permette di visualizzare le bande
