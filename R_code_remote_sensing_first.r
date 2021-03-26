4b5d36ab78cf79feda45c55f9cb6071b4c061a55
TLBacchilega <80838223+TLBacchilega@users.noreply.github.com>
2021-03-26 20:04:22 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,4 +1,4 @@
-# My frst code in R for remote sensing
+# My first code in R for remote sensing
 # creazione della cartella di riferimento lab sul disco principale C 
 install.packages("raster") # (argomento)
 library(raster)
@@ -7,7 +7,7 @@ p224r63_2011 <- brick("p224r63_2011_masked.grd") # funzione brick permette di im
 p224r63_2011 #info sull'immagine
 plot(p224r63_2011) # funzione che permette di visualizzare le bande
 
-#### Day2
+## Day2
 
 # colour change
 cl <- colorRampPalette(c('black','grey','light grey'))(100) #(100) livelli 
@@ -16,7 +16,7 @@ plot(p224r63_2011, col=cl)
 cl <- colorRampPalette(c("green","blue","light grey"))(100)
 plot(p224r63_2011, col=cl)
 
-# Day3
+### Day3
 
 ## Bande Landsat
 # B1: blu
@@ -82,3 +82,49 @@ plot(p224r63_2011$B3_sre, col=clr)
 
 clnir <- colorRampPalette(c("red","orange","yellow")) (100)
 plot(p224r63_2011$B4_sre, col=clnir)
+
+#### Day4
+
+## Visualizing data by RGB plotting
+
+
+# Bande Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
+
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+# Exercise: mount a 2x2 multiframe
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+# https://www.rdocumentation.org/packages/grDevices/versions/3.6.2/topics/pdf
+pdf("il_mio_primo_pdf_con_R.pdf")
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+dev.off()
+
+
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
+
+
+# par natural colours, flase colours, and false colours with histogram stretching
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
