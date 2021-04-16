3f4e77254b7233373a940c78dcd1babed99678b0
TLBacchilega <80838223+TLBacchilega@users.noreply.github.com>
2021-04-16 15:04:08 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -4,3 +4,12 @@
 setwd("C:/lab/greenland")
 install.packages("knitr") 
 library(knitr) # require(knitr)
+
+# https://www.rdocumentation.org/packages/knitr/versions/1.31/topics/stitch
+
+setwd("C:/lab/")  
+
+# stitch("~/Downloads/R_code_temp.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+stitch("R_code_greenland.r.tx", template=system.file("misc", "knitr-template.Rnw", package="knitr")) # documento di testo salvato nella cartella lab: R_code_greenland.r
+
+# https://yihui.org/tinytex/r/#debugging
