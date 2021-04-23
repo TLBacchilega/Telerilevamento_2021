58b2fdbaf152dbb9b5eb87ce4eb79c3178b8ca21
TLBacchilega <80838223+TLBacchilega@users.noreply.github.com>
2021-04-23 10:10:55 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -26,6 +26,14 @@ library(RStoolbox) # pacchetto già installato
 soc <- unsuperClass(so, nClasses=3) # oggetto soc
 plot(soc$map)
 
+
+soc20 <- unsuperClass(so, nClasses=20)
+plot(soe20$map,col=cl)
+
+cl <- colorRampPalette(c('yellow','black','red'))(100)
+plot(soc20$map,col=cl)
+
+
 # Unsupervised Classification with 20 classes
 soc <- unsuperClass(so, nClasses=20)
 plot(soc$map)
@@ -51,6 +59,30 @@ plot(soc20$map)
 lic <- brick("lic.png.JPG") # immagine Cladonia sp.
 lic <- unsuperClass(lic, nClasses=3)
 plot(lic$map)
+
+
+
+# Grand Canyon
+# https://landsat.visibleearth.nasa.gov/view.php?id=80948
+
+# When John Wesley Powell led an expedition down the Colorado River and through the Grand Canyon in 1869, he was confronted with a daunting landscape. At its highest point, the serpentine gorge plunged 1,829 meters (6,000 feet) from rim to river bottom, making it one of the deepest canyons in the United States. In just 6 million years, water had carved through rock layers that collectively represented more than 2 billion years of geological history, nearly half of the time Earth has existed.
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+
+# https://cran.r-project.org/web/packages/RStoolbox/RStoolbox.pdf
+
+
+gcc2 <- unsuperClass(gc, nClasses=2) # nClass argomento fondamentale
+gcc2
+plot(gcc2$map)
+
+gcc4 <- unsuperClass(gc, nClasses=4) # gc nome immagine originale
+plot(gcc4$map)
+
 
  
 
