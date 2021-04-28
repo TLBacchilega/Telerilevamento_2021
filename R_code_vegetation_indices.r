1654906e5d776db19e006bf1d7e0e2df7695dce7
TLBacchilega <80838223+TLBacchilega@users.noreply.github.com>
2021-04-28 15:27:41 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -1,11 +1,13 @@
 # R_code_vegetation_indices.r
 
-library(raster)
+library(raster) # pacchetto già scaricato
 
 setwd("C:/lab/") # Windows
 
 defor1<-brick("defor1.jpg")
 defor2 <- brick("defor2.jpg")
+
+# b1 = NIR, b2 = red, b3 = green
 
 par(mfrow=c(2,1))
 plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
